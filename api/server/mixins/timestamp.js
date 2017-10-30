4184f91f7e1b8cc634021e561e5c51baacb38ca9
lakisov <lakisov@singree.com>
2017-10-30 23:43:35 +0200

    soft delete

--- api/server/mixins/timestamp.js
+++ api/server/mixins/timestamp.js
@@ -1,52 +1,82 @@
 'use strict';
 
+const Promise = require('bluebird');
+
 module.exports = (Model, bootOptions = {}) => {
   const options = {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at',
-    deletedAt: 'deleted_at',
+    createdKey: 'created_at',
+    updatedKey: 'updated_at',
+    deletedKey: 'deleted_at',
     required: true,
     validateUpsert: false,
     silenceWarnings: false,
     ...bootOptions
   };
 
-  Model.defineProperty(options.createdAt, {
+  Model.defineProperty(options.createdKey, {
     type: Date,
     required: options.required,
     defaultFn: 'now'
   });
 
-  Model.defineProperty(options.updatedAt, {
+  Model.defineProperty(options.updatedKey, {
     type: Date,
     required: options.required,
     defaultFn: 'now'
   });
 
-  Model.defineProperty(options.deletedAt, {
+  Model.defineProperty(options.deletedKey, {
     type: Date,
     required: false
   });
 
-  Model.observe('before save', (ctx, next) => {
-    if (ctx.options && ctx.options.skipUpdatedAt) {
-      return next();
-    }
-    if (ctx.instance) {
-      ctx.instance[options.updatedAt] = new Date();
+  Model.destroyAllForce  = Model.destroyAll;
+  Model.destroyByIdForce = Model.destroyById;
+  let destroy = Model.prototype.destroy;
+
+  Model.destroyAll = (where, cb) => {
+    return Model.updateAll(where, {[options.deletedKey]: new Date()})
+      .then(result => (typeof cb === 'function') ? cb(null, result) : result)
+      .catch(error => (typeof cb === 'function') ? cb(error) : Promise.reject(error));
+  };
+
+  Model.destroyById = function softDestroyById(id, cb) {
+    return Model.updateAll({id}, {[options.deletedKey]: new Date()})
+      .then(result => (typeof cb === 'function') ? cb(null, result) : result)
+      .catch(error => (typeof cb === 'function') ? cb(error) : Promise.reject(error));
+  };
+
+  Model.prototype.destroy = (options, cb) => {
+    if (options && options.force) {
+      destroy(options, cb);
     } else {
-      ctx.data[options.updatedAt] = new Date();
+      const callback = (cb === undefined && typeof options === 'function') ? options : cb;
+
+      return this.updateAttributes({[options.deletedKey]: new Date()})
+        .then(result => (typeof cb === 'function') ? callback(null, result) : result)
+        .catch(error => (typeof cb === 'function') ? callback(error) : Promise.reject(error));
+    }
+  };
+
+  Model.observe('access', (ctx, next) => {
+    if (!ctx.query.where) {
+      ctx.query.where = {};
+    }
+    if (!ctx.query.where.hasOwnProperty(options.deletedKey) && !ctx.query.paranoid && ctx.Model.definition.properties[options.deletedKey]) {
+      ctx.query.where[options.deletedKey] = null;
     }
     return next();
   });
 
-  Model.observe('before delete', (ctx, next) => {
-    Model.updateAll(ctx.where, {[options.deletedAt]: new Date()})
-      .then((result) => {
-        next(null);
-      })
-      .catch(err => {
-        return next(err);
-      });
+  Model.observe('before save', (ctx, next) => {
+    if (ctx.options && ctx.options.skipupdatedKey) {
+      return next();
+    }
+    if (ctx.instance) {
+      ctx.instance[options.updatedKey] = new Date();
+    } else {
+      ctx.data[options.updatedKey] = new Date();
+    }
+    return next();
   });
 };
