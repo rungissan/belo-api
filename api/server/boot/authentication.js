033eddecea3ba4ddc29be462d1aa8647ef5ee6c2
lakisov4singree <lakisov@singree.com>
2017-11-02 19:22:31 +0200

    WIP testing oauth2 loopback component

--- api/server/boot/authentication.js
+++ api/server/boot/authentication.js
@@ -1,6 +1,34 @@
 'use strict';
 
-module.exports = function enableAuthentication(server) {
+module.exports = function enableAuthentication(app) {
   // enable authentication
-  server.enableAuth();
+  var oauth2 = require('loopback-component-oauth2');
+
+  var options = {
+    dataSource: app.dataSources.postgres, // Data source for oAuth2 metadata persistence
+    loginPage: '/login_auth3', // The login page url
+    loginPath: '/login_auth', // The login form processing url
+    resourceServer: true,
+    // session: false
+  };
+
+  oauth2.oAuth2Provider(
+    app, // The app instance
+    options // The options
+  );
+
+  var auth = oauth2.authenticate({session: false, scope: 'demo'});
+  app.use(['/protected', '/api', '/me'], auth);
+
+  /* jshint unused: vars */
+  app.get('/me', function(req, res, next) {
+    // req.authInfo is set using the `info` argument supplied by
+    // `BearerStrategy`.  It is typically used to indicate scope of the token,
+    // and used in access control checks.  For illustrative purposes, this
+    // example simply returns the scope in the response.
+    res.json({ 'user_id': req.user.id, name: req.user.username,
+      accessToken: req.authInfo.accessToken });
+  });
+
+  app.enableAuth();
 };
