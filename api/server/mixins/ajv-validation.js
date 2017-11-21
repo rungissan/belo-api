c5a407ee24971dfb92b9a2d8a6111b23cf8d22a0
lakisov4singree <lakisov@singree.com>
2017-11-21 12:54:31 +0200

    add ajv validation source setting

--- api/server/mixins/ajv-validation.js
+++ api/server/mixins/ajv-validation.js
@@ -1,26 +1,41 @@
 'use strict';
 
 const debug = require('debug')('spiti:validations');
+import Promise  from 'bluebird';
 
 import validate from '../lib/validate';
-
-const Promise = require('bluebird');
 
 export default function(Model, options = {}) {
   const modelConfig = Model.settings;
   let remoteHooks = options.remoteHooks || {};
 
   Object.keys(remoteHooks).forEach(remoteName => {
-    if (typeof remoteHooks[remoteName] == 'string') {
-      Model.beforeRemote(remoteName, getValidteMiddleware(Model.modelName, remoteHooks[remoteName]));
-    }
+    Model.beforeRemote(remoteName, getValidteMiddleware(Model.modelName, remoteHooks[remoteName]));
   });
 };
 
-function getValidteMiddleware(modelName, schemaName) {
+function getValidteMiddleware(modelName, options) {
+  let schemaName;
+  let instancePath;
+  if (typeof options == 'string') {
+    schemaName = options;
+  } else if (typeof options == 'object') {
+    if (typeof options.args == 'string') {
+      instancePath = options.args;
+    }
+    schemaName = options.schema;
+  }
+
   return async function validateMiddleware(ctx, modelInstance) {
-    debug(`Validating by model: ${modelName}, schema ${schemaName}`);
-    let instance = ctx.args.instance || ctx.args.data;
+    debug(`Validating by model: ${modelName}, ${schemaName}`);
+
+    let instance;
+    if (instancePath) {
+      instance = ctx.args[instancePath];
+    } else {
+      instance = ctx.args.instance || ctx.args.data;
+    }
+
     return await validate(instance, modelName, schemaName);
   };
 }
