ef6206e4a3875eaf4d9f46601127eaf64d1a690f
lakisov <lakisov@singree.com>
2017-12-03 16:49:47 +0200

    add m-to-m post locations

--- api/sequelize/migrations/0005_post_and_listing.js
+++ api/sequelize/migrations/0005_post_and_listing.js
@@ -59,12 +59,12 @@ const post = (DataTypes) => ({
     references: {model: {tableName: 'attachment', ...BASE_SCHEMA}},
     ...CASCADE_RULES
   },
-  geolocation_id: {
-    type: DataTypes.INTEGER,
-    references: {model: {tableName: 'geolocation', ...BASE_SCHEMA}},
-    ...CASCADE_RULES,
-    allowNull: false
-  },
+  // geolocation_id: {
+  //   type: DataTypes.INTEGER,
+  //   references: {model: {tableName: 'geolocation', ...BASE_SCHEMA}},
+  //   ...CASCADE_RULES,
+  //   allowNull: false
+  // },
   listing_id: {
     type: DataTypes.INTEGER,
     references: {model: {tableName: 'listing', ...BASE_SCHEMA}},
@@ -107,12 +107,29 @@ const attachment_to_post = (DataTypes) => ({
   }
 });
 
+const geolocation_to_post = (DataTypes) => ({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  geolocation_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {model: {tableName: 'geolocation', ...BASE_SCHEMA}},
+    ...CASCADE_RULES
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {model: {tableName: 'post', ...BASE_SCHEMA}},
+    ...CASCADE_RULES
+  }
+});
+
 module.exports = {
   up: (queryInterface, DataTypes) => {
     return queryInterface.createTable('listing', listing(DataTypes), BASE_SCHEMA)
       .then(() => queryInterface.createTable('post', post(DataTypes), BASE_SCHEMA))
       .then(() => queryInterface.createTable('attachment_to_post', attachment_to_post(DataTypes), BASE_SCHEMA))
       .then(() => queryInterface.createTable('attachment_to_listing', attachment_to_listing(DataTypes), BASE_SCHEMA))
+      .then(() => queryInterface.createTable('geolocation_to_post', geolocation_to_post(DataTypes), BASE_SCHEMA))
       .then(() => queryInterface.addIndex(
         `${BASE_SCHEMA.schema}.attachment_to_post`,
         ['attachment_id', 'post_id'],
@@ -126,11 +143,19 @@ module.exports = {
         {
           indicesType: 'UNIQUE'
         }
+      ))
+      .then(() => queryInterface.addIndex(
+        `${BASE_SCHEMA.schema}.geolocation_to_post`,
+        ['geolocation_id', 'post_id'],
+        {
+          indicesType: 'UNIQUE'
+        }
       ));
   },
   down: (queryInterface) => {
     return queryInterface.dropTable('attachment_to_listing')
       .then(() => queryInterface.dropTable('attachment_to_post'))
+      .then(() => queryInterface.dropTable('geolocation_to_post'))
       .then(() => queryInterface.dropTable('post'))
       .then(() => queryInterface.dropTable('listing'));
   }
