8e3eaaa1509b3b47867a4bdcb3dfdf80fe75b195
lakisov4singree <lakisov@singree.com>
2017-12-08 14:32:38 +0200

    fix db export script

--- bin/db/db-export-parallel.sh
+++ bin/db/db-export-parallel.sh
@@ -15,7 +15,7 @@ BACKUP_NAME=/tmp/${1:-spt_testing}_$(date +%Y%m%d)_backup
 
 docker exec spiti_postgres_1 pg_dump -U admin -Fd spiti -j 4 -f $BACKUP_NAME
 
-cd ../../database-dumps/
+cd ~/database-dumps/
 docker cp spiti_postgres_1:$BACKUP_NAME .
 
 docker exec spiti_postgres_1 bash -c "rm -rf $BACKUP_NAME"
