6698bba5cb6d2366ee12b2c359bc086766238c3a
lakisov4singree <lakisov@singree.com>
2017-12-22 16:59:33 +0200

    WIP add open-house images ref

---
+++ api/sequelize/migrations/0011_open_house_images.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import {
+  defaultFields,
+  CASCADE_RULES,
+  BASE_SCHEMA
+} from '../utils';
+
+const attachment_to_open_house = (DataTypes) => ({
+  id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+  attachmentId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {model: {tableName: 'attachment', ...BASE_SCHEMA}},
+    ...CASCADE_RULES
+  },
+  openHouseId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {model: {tableName: 'open_house', ...BASE_SCHEMA}},
+    ...CASCADE_RULES
+  }
+});
+
+module.exports = {
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable('attachment_to_open_house', attachment_to_open_house(DataTypes), BASE_SCHEMA)
+      .then(() => queryInterface.addIndex(
+        `${BASE_SCHEMA.schema}.attachment_to_open_house`,
+        ['attachmentId', 'openHouseId'],
+        {
+          indicesType: 'UNIQUE'
+        }
+      ));
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable({
+      tableName: 'attachment_to_open_house',
+      ...BASE_SCHEMA
+    });
+  }
+};
