e0c4c695f011c09ba41bc1a00bd689372ce9a71a
Ganas Alexander <ganas@singree.com>
2018-08-10 12:02:09 +0300

    reworked some flow for appointments

--- api/server/models/appointment.js
+++ api/server/models/appointment.js
@@ -3,23 +3,68 @@ module.exports = function(Appointment) {
     Appointment.searchOrCreate = async function(ctx, appnt) {
 
         // status: 0 - requesting, 1 - accepted, 2 - canceled by requester
-        
-        const data = await Appointment.findOrCreate({
-            where: { 
-                userId: appnt.userId,
-                feedId: appnt.feedId,
-                listingOwnerId: appnt.listingOwnerId,
-                status: 0
-            }
+
+        const data = await Appointment.upsertWithWhere({
+            userId: appnt.userId,
+            feedId: appnt.feedId,
+            listingOwnerId: appnt.listingOwnerId,
         }, {
             userId: appnt.userId,
             feedId: appnt.feedId,
             listingOwnerId: appnt.listingOwnerId,
+            lastTimeUpdateBy: appnt.userId,
+            status: 0,
             date: appnt.date,
-            time: appnt.time,
-            lastTimeUpdateBy: appnt.userId
-        })
-        return data[0];
+            time: appnt.time
+        });
+
+        // const data = await Appointment.findOrCreate({
+        //     where: { 
+        //         userId: appnt.userId,
+        //         feedId: appnt.feedId,
+        //         listingOwnerId: appnt.listingOwnerId
+        //     }
+        // }, {
+        //     userId: appnt.userId,
+        //     feedId: appnt.feedId,
+        //     listingOwnerId: appnt.listingOwnerId,
+        //     date: appnt.date,
+        //     time: appnt.time,
+        //     lastTimeUpdateBy: appnt.userId
+        // })
+
+        // if(data[0].status === 2){
+        //     const updatedAppnt = await Appointment.upsertWithWhere({
+        //         or: [
+        //             {
+        //                 userId: appnt.userId,
+        //                 feedId: appnt.feedId,
+        //                 listingOwnerId: appnt.listingOwnerId,
+        //                 status: 2
+        //             },
+        //             {
+        //                 userId: appnt.userId,
+        //                 feedId: appnt.feedId,
+        //                 listingOwnerId: appnt.listingOwnerId,
+        //                 status: 0
+        //             }
+        //         ]
+        //     }, {
+        //         userId: appnt.userId,
+        //         feedId: appnt.feedId,
+        //         listingOwnerId: appnt.listingOwnerId,
+        //         lastTimeUpdateBy: appnt.userId,
+        //         status: 0,
+        //         date: appnt.date,
+        //         time: appnt.time
+        //     });
+        //     console.log(updatedAppnt)
+        //     return updatedAppnt;
+        // }
+
+        console.log(data)
+
+        return data;
     }
 
     Appointment.remoteMethod(
