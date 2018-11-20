3f49ffa2f055f7286e910e410be060bba3773b77
Yury Samoshkin <samoshkin@singree.com>
2018-11-20 15:27:46 +0200

    styling

--- api/server/boot/automigrate.js
+++ api/server/boot/automigrate.js
@@ -4,21 +4,18 @@ module.exports = (app) => {
   var postgres = app.dataSources.postgres;
   const tables = [];
   console.log('-- Models found:', Object.keys(tables));
- ``
-
-   tables.forEach(model => {
+  tables.forEach(model => {
     console.log('Cheking if table for model ' + model + ' is created and up-to-date in DB...');
     console.log(model);
-   
     postgres.isActual(model, (err, actual) => {
       if (actual) {
         console.log('Model ' + model + ' is up-to-date. No auto-migrated.');
       } else {
         console.log('Difference found! Auto-migrating model ' + model + '...');
         postgres.autoupdate(model, () => {
-           console.log('Auto-migrated model ' + model + ' successfully.');
+          console.log('Auto-migrated model ' + model + ' successfully.');
         });
       }
     });
   });
- };
+};
--- api/server/models/chat.js
+++ api/server/models/chat.js
@@ -19,7 +19,7 @@ module.exports = function(Chat) {
     let query = {
       where: {
         fromId: user.id
-      }     
+      }
     };
     const chatSearch = new ChatSearch(Chat.app.dataSources.postgres.connector, Chat.app, {baseModelName: 'Chat'});
     let chats = await chatSearch.queryChats(query);
@@ -93,11 +93,9 @@ module.exports = function(Chat) {
   async function getMessages(socket, data = {}) {
     let { user } = socket;
     let { chatId, earlierThanId, limit, offset, order, include } = data;
-       
     const { ChatMessage, ChatToAccount, Followed} = Chat.app.models;
 
     const linkedAccounts = await ChatToAccount.find({where: {chatId}});
- 
     if (!linkedAccounts) {
       throw errUnauthorized();
     }
@@ -105,10 +103,10 @@ module.exports = function(Chat) {
       .filter(item => item.userId != user.id)
       .map(async item => {
         let isFollowed = await Followed.findOne({where: { userId: item.userId, followedId: user.id}})
-        if (isFollowed) { return item.userId } 
+        if (isFollowed) { return item.userId; }
         return null;
-       }));
-   
+      }));
+
     let query = {
       where: { chatId },
       limit: limit || 10,
@@ -126,9 +124,9 @@ module.exports = function(Chat) {
     }
 
     let chats = await ChatMessage.find(query);
-    chats.map(item => { 
-        return item.__data.account.__data.isFollowed = followedUserIds.includes(item.userId) ? true: false
-       });
+    chats.map(item => {
+      return item.__data.account.__data.isFollowed = followedUserIds.includes(item.userId) ? true : false;
+    });
 
     return chats;
   };
@@ -211,7 +209,7 @@ module.exports = function(Chat) {
       throw errValidation('message required');
     }
 
-    const { ChatMessage, ChatToAccount,Followed} = Chat.app.models;
+    const { ChatMessage, ChatToAccount, Followed} = Chat.app.models;
 
     const linkedAccount = await ChatToAccount.findOne({
       where: {
@@ -228,7 +226,6 @@ module.exports = function(Chat) {
       throw errUnauthorized();
     }
 
-
     const createdMessage = await ChatMessage.create({
       chatId,
       userId: user.id,
@@ -237,24 +234,22 @@ module.exports = function(Chat) {
     await linkedAccount.updateAttributes({lastReadedMessageId: createdMessage.id});
 
     const participantsAccounts = await ChatToAccount.find({where: {chatId}});
-    console.log(participantsAccounts);  
- 
+
     const followedUserIds = await Promise.all(participantsAccounts
        .filter(item => item.userId != user.id)
        .map(async item => {
          let isFollowed = await Followed.findOne({where: { userId: item.userId, followedId: user.id}})
-         if (isFollowed) { return item.userId } 
+         if (isFollowed) { return item.userId; }
          return null;
-        }));
-     console.log(followedUserIds);  
+       }));
 
     const transformedData = {...linkedAccount.toJSON(), ...createdMessage.toJSON()}
-        
-     // check if multichat
-    if (followedUserIds.length > 1){
-       transformedData.account.multichat = followedUserIds;
-    }  else  transformedData.account.isFollowed = followedUserIds[0] ? true: false;
- 
+
+    // check if multichat
+    if (followedUserIds.length > 1) {
+      transformedData.account.multichat = followedUserIds;
+    }  else  transformedData.account.isFollowed = followedUserIds[0] ? true : false;
+
     socket.to(getRoomName(chatId)).emit('message', 'messageCreated', transformedData);
     return transformedData;
   };
