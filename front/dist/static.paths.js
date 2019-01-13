dc111b0428b9484c82982e9eaac74a6403e91a92
iurii <yury@samoshk.in>
2019-01-13 22:31:13 +0200

    pwa && robots

--- front/dist/static.paths.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROUTES = [
-    '/',
-    '/lazy',
-    '/lazy/nested'
-];
-//# sourceMappingURL=static.paths.js.map
